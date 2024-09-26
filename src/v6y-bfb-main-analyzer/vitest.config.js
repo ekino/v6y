7e80ce6387d18452a802ee4c7ba17f611af1b262
hela.ben-khalfallah <helabenkhalfallah@hotmail.fr>
2024-09-26 11:57:30 +0200

    [EVOL]: migrate jest to vitest

---
+++ src/v6y-bfb-main-analyzer/vitest.config.js
@@ -0,0 +1,20 @@
+import { configDefaults, defineConfig } from 'vitest/config';
+
+/**
+ * https://vitest.dev/config/#configuration
+ */
+export default defineConfig({
+    test: {
+        environment: 'jsdom',
+        exclude: [...configDefaults.exclude],
+        include: [
+            ...configDefaults.include,
+            'src/**/*.{test,spec}.?(c|m)[jt]s?(x)',
+            'src/**/*-{test,spec}.?(c|m)[jt]s?(x)',
+        ],
+        coverage: {
+            include: ['src/**'],
+            exclude: [...configDefaults.coverage.exclude, '**/AppLogger.js'],
+        },
+    },
+});
