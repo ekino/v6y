edec7e671695a7ec56b2c45226eddbf85b6b2fb1
Olga Yasnopolskaya <olga.yasnopolskaya@ekino.com>
2024-11-25 14:29:56 +0100

    [FIX]: typed the return of dynamic component

--- src/v6y-front/src/commons/components/VitalityDynamicLoader.tsx
+++ src/v6y-front/src/commons/components/VitalityDynamicLoader.tsx
@@ -4,11 +4,10 @@ interface DynamicComponentProps {
     [key: string]:  unknown; 
 }
 
-
 const DynamicComponent = (path : string) => buildPathConfig(path);
 
 const VitalityDynamicLoader = (path: string): React.ComponentType<DynamicComponentProps> => {
-    return DynamicComponent(path);
+    return DynamicComponent(path) as React.ComponentType<DynamicComponentProps>;
 }
 
 export default VitalityDynamicLoader;
