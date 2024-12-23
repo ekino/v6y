e36f295c8d90d91f914ed7095e7fb1165b8463b3
Olga Yasnopolskaya <olga.yasnopolskaya@ekino.com>
2024-12-23 18:24:12 +0100

    [REFACTOR]: formatted the code

--- src/v6y-front-bo/src/infrastructure/adapters/api/GraphQLClient.ts
+++ src/v6y-front-bo/src/infrastructure/adapters/api/GraphQLClient.ts
@@ -1,17 +1,20 @@
 import { GraphQLClient } from 'graphql-request';
 import Cookies from 'js-cookie';
 
-export const gqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_V6Y_GQL_API_BASE_PATH as string, {
-    fetch: (url: RequestInfo | URL, options?: RequestInit) => {
-        return fetch(url, {
-            ...options,
-            headers: {
-                ...(options?.headers || {}),
-                Authorization: `Bearer ${JSON.parse(Cookies.get('auth') || '{}')?.token}`,
-            },
-        });
+export const gqlClient = new GraphQLClient(
+    process.env.NEXT_PUBLIC_V6Y_GQL_API_BASE_PATH as string,
+    {
+        fetch: (url: RequestInfo | URL, options?: RequestInit) => {
+            return fetch(url, {
+                ...options,
+                headers: {
+                    ...(options?.headers || {}),
+                    Authorization: `Bearer ${JSON.parse(Cookies.get('auth') || '{}')?.token}`,
+                },
+            });
+        },
     },
-});
+);
 
 type GqlClientRequestParams = {
     gqlQueryPath?: string;
--- src/v6y-front-bo/src/infrastructure/components/RefineEditWrapper.tsx
+++ src/v6y-front-bo/src/infrastructure/components/RefineEditWrapper.tsx
@@ -16,6 +16,9 @@ import { FormWrapperProps } from '../types/FormType';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 export default function RefineEditWrapper({
     title,
     queryOptions,
