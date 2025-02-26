dfdb774fa2e2b1906f174d29be61cbe9da4ff53f
Maël AUBERT <mael.aubert@ekino.com>
2025-02-26 14:59:35 +0100

    fix: added jsdoc

--- v6y-libs/core-logic/src/core/DateUtils.ts
+++ v6y-libs/core-logic/src/core/DateUtils.ts
@@ -1,5 +1,16 @@
+/**
+ * Type representing the supported date string formats.
+ * - 'YYYY-MM-DD': Date formatted as 'YYYY-MM-DD'.
+ * - 'ISO': Date formatted as ISO string.
+ */
 type DateStringFormatType = 'YYYY-MM-DD' | 'ISO';
 
+/**
+ * Formats a Date object to a string based on the specified format.
+ *
+ * @param date - The Date object to format.
+ * @param format - The format to use for the date string. Defaults to 'ISO'.
+ */
 const formatDateToString = (date: Date, format?: DateStringFormatType): string => {
     if (!format) {
         format = 'ISO';
@@ -14,10 +25,18 @@ const formatDateToString = (date: Date, format?: DateStringFormatType): string =
     }
 };
 
+/**
+ * Parses a date string into a Date object.
+ *
+ * @param dateStr - The date string to parse.
+ */
 const formatStringToDate = (dateStr: string): Date => {
     return new Date(dateStr);
 };
 
+/**
+ * DateUtils module providing utility functions for date formatting and parsing.
+ */
 const DateUtils = {
     formatDateToString,
     formatStringToDate,
