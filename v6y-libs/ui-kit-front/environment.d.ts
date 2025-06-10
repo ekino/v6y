2429f74dde9265c0a3f5aa32782c1d9c79882782
Maël AUBERT <maelaubert56@gmail.com>
2025-06-10 15:28:43 +0200

    [FEAT]: setup new ui-kit-front with tailwind and shadcn components

---
+++ v6y-libs/ui-kit-front/environment.d.ts
@@ -0,0 +1,9 @@
+import 'next';
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            [key: string]: string | undefined;
+        }
+    }
+}
---
+++ v6y-libs/ui-kit-front/src/index.ts
@@ -0,0 +1,17 @@
+// // Styles
+// import './styles.css';
+
+// Components
+export * from './components/ui/button';
+export type * from './components/ui/button';
+export * from './components/ui/input';
+export * from './components/ui/checkbox';
+export * from './components/ui/form';
+export * from './components/ui/label';
+export * from './components/ui/card';
+export * from './components/ui/alert';
+export * from './components/ui/sonner';
+export * from './components/Typography';
+
+// Utils
+export { cn } from './lib/utils';
