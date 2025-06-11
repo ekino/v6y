6ffcabacb50f0251e312228efcac0832cb529099
Maël AUBERT <maelaubert56@gmail.com>
2025-06-11 17:22:19 +0200

    [FEAT]: changed login page and header to the new ui-kit

--- v6y-apps/front/src/commons/components/layout/VitalityPageLayout.tsx
+++ v6y-apps/front/src/commons/components/layout/VitalityPageLayout.tsx
@@ -3,12 +3,12 @@
 import {
     Col,
     FloatButton,
-    Layout,
     Row,
     TitleView,
     useNavigationAdapter,
     useTranslationProvider,
 } from '@v6y/ui-kit';
+import { Toaster } from '@v6y/ui-kit-front';
 import * as React from 'react';
 import { ReactNode } from 'react';
 
@@ -19,37 +19,29 @@ import VitalityBreadcrumb from './VitalityBreadcrumb';
 import VitalityPageFooter from './VitalityPageFooter';
 import VitalityPageHeader from './VitalityPageHeader';
 
-const { Header, Footer, Content } = Layout;
-
 const VitalityPageLayout = ({ children }: { children: ReactNode }) => {
     const { pathname } = useNavigationAdapter();
     const { translate } = useTranslationProvider();
     const pageTitle = buildPageTitle(pathname, translate);
 
     return (
-        <Layout>
-            <Header>
-                <VitalityPageHeader title="VITALITY" subTitle="v6y" />
-            </Header>
-            <Content>
-                <ProtectedRoute>
-                    <Row justify="center" align="middle" gutter={[12, 12]}>
-                        <Col span={23}>
-                            <VitalityBreadcrumb />
-                        </Col>
-                        <Col span={20}>
-                            {pageTitle && <TitleView title={pageTitle as string} />}
-                        </Col>
-                        <Col span={18}>{children}</Col>
-                    </Row>
-                </ProtectedRoute>
-            </Content>
-            <Footer>
-                <VitalityPageFooter />
-            </Footer>
+        <>
+            <VitalityPageHeader />
+            <ProtectedRoute>
+                <Row justify="center" align="middle" gutter={[12, 12]}>
+                    <Col span={23}>
+                        <VitalityBreadcrumb />
+                    </Col>
+                    <Col span={20}>{pageTitle && <TitleView title={pageTitle as string} />}</Col>
+                    <Col span={18}>{children}</Col>
+                </Row>
+            </ProtectedRoute>
+
+            <VitalityPageFooter />
             <VitalityBot />
             <FloatButton.BackTop />
-        </Layout>
+            <Toaster position="top-center" richColors />
+        </>
     );
 };
 
