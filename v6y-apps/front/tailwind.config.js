614a35c9a315ac54b14180269ccb68f3601515f3
Maël AUBERT <maelaubert56@gmail.com>
2025-06-11 11:03:17 +0200

    fix: update dependencies and remove unused style.css. Ui-kit structure is done

--- v6y-apps/front/tailwind.config.js
+++ v6y-apps/front/tailwind.config.js
@@ -1,15 +1,11 @@
-const { theme, plugins } = require('@v6y/ui-kit-front/tailwind.config.js');
+const {join} = require('path');
+const uiConfig = require('@v6y/ui-kit-front/tailwind.config.js');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  ...uiConfig,
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
+    join(__dirname, 'src/**/*.{js,ts,jsx,tsx}'),
+    ...uiConfig.content,
   ],
-  theme: {
-    ...theme,
-    extend: {},
-  },
-  plugins: [
-    ...plugins,
-  ],
-};
+}
