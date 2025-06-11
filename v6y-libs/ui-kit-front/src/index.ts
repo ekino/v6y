682e5373d9b05c7bef7b5d5d74dee1309a7b7478
Maël AUBERT <maelaubert56@gmail.com>
2025-06-11 17:18:18 +0200

    [REFACTOR]: moved components to atoms and organisms folders. Setuped translation provider Added new components

--- v6y-libs/ui-kit-front/src/index.ts
+++ v6y-libs/ui-kit-front/src/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Styles
 import './styles.css';
 
@@ -15,23 +14,3 @@ export * from './hooks';
 
 // Utils
 export { cn } from './lib/utils';
-||||||| parent of 6cf211b ([FEAT]: setup new ui-kit-front with tailwind and shadcn components)
-=======
-// // Styles
-// import './styles.css';
-
-// Components
-export * from './components/ui/button';
-export type * from './components/ui/button';
-export * from './components/ui/input';
-export * from './components/ui/checkbox';
-export * from './components/ui/form';
-export * from './components/ui/label';
-export * from './components/ui/card';
-export * from './components/ui/alert';
-export * from './components/ui/sonner';
-export * from './components/Typography';
-
-// Utils
-export { cn } from './lib/utils';
->>>>>>> 6cf211b ([FEAT]: setup new ui-kit-front with tailwind and shadcn components)
